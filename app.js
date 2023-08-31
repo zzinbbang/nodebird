a9325569da9219a2c2d0224cb483d8549db2c266
Arin <52065397+zzinbbang@users.noreply.github.com>
2023-08-31 14:15:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const pageRouter = require('./routes/page');
 const authRouter = require('./routes/auth');
 const postRouter = require('./routes/post');
 const userRouter = require('./routes/user');
+const profileRouter = require('./routes/profile');
 const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
@@ -53,6 +54,7 @@ app.use('/', pageRouter);
 app.use('/auth', authRouter);
 app.use('/post', postRouter);
 app.use('/user', userRouter);
+app.use('/profile', profileRouter);
 
 app.use((req, res, next) => {
   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
